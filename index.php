93b106284012c7d6105f2fe46ee6e49215941161
Vlad <programmist.vke@yandex.ru>
2017-11-09 16:41:38 +0300

    float to string

--- index.php
+++ index.php
@@ -17,8 +17,9 @@ $tomorrow = strtotime('tomorrow midnight');
 $now = strtotime('now');
 
 $diff = $tomorrow - $now;
-$hours = str_pad(floor($diff / 3600), 2, "0", STR_PAD_LEFT);
-$minutes = str_pad(floor($diff / 60) - ($hours * 60), 2, "0", STR_PAD_LEFT);
+$hours = (string) str_pad(floor($diff / 3600), 2, "0", STR_PAD_LEFT);
+$minutes = (string) str_pad(floor($diff / 60) - ($hours * 60), 2, "0", STR_PAD_LEFT);
+var_dump($minutes);
 
 // далее нужно вычислить оставшееся время до начала следующих суток и записать его в переменную $lot_time_remaining
 $lot_time_remaining = $hours.':'.$minutes;
