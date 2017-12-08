e453175c5c65dc06f095082fb8579d9483b8b14f
Vlad <programmist.vke@yandex.ru>
2017-12-08 20:02:42 +0300

    Удаление комментария

--- functions.php
+++ functions.php
@@ -22,24 +22,3 @@ function include_template($template_name, $data) {
        return '';
     }
 }
-
-//function error_404 () {
-//    http_response_code(404);
-//}
-
-//function show_error ($title, $message) {
-//    $page_data = [
-//        'categories' => $categories,
-//        'title' => $title,
-//        'message' => $message,
-//    ];
-//    $page_content = include_template('error', $page_data);
-//    $layout_content = include_template('layout', [
-//        'content' => $page_content,
-//        'title' => $title,
-//        'is_auth' => $is_auth,
-//        'user_name' => $user_name,
-//        'user_avatar' => $user_avatar
-//    ]);
-//    print($layout_content);
-//}
