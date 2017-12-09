491d4cf10c7882a2db03ed96f227e97ac1421586
Vlad <programmist.vke@yandex.ru>
2017-12-09 19:16:08 +0300

    Хранение данных между визитами

--- lot.php
+++ lot.php
@@ -16,14 +16,52 @@ if (!$page_found) {
     ];
     $page_content = include_template('error', $page_data);
 } else {
+    $is_bet = false;
+    $cookie_value = [];
+    
+    if (isset($_COOKIE['bets'])) {
+        $cookie_value = json_decode($_COOKIE['bets'], true);
+    }
+
+    if (isset($cookie_value[$lot_id])) {
+        $is_bet = true;
+    }
+
     $page_data = [
         'categories' => $categories,
         'lot' => $announcements[$lot_id],
-        'bets' => $bets
+        'bets' => $bets,
+        'lot_id' => ($lot_id >= 0) ? $lot_id : '',
+        'is_bet' => $is_bet
     ];
     $page_title = $announcements[$lot_id]['name'];
     $page_content = include_template('lot', $page_data);
 }
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $cost = (isset($_POST['cost'])) ? $_POST['cost'] : '';
+    $bet = [
+        'cost' => $cost,
+        'time' => time()
+    ];
+
+    $cookie_name = 'bets';
+    $cookie_value = [];
+    $expire = strtotime("+30 days");
+    $path = "/";
+
+    if (isset($_COOKIE['bets'])) {
+        $cookie_value = json_decode($_COOKIE['bets'], true);
+    }
+
+    $cookie_value[$lot_id] = $bet;
+    setcookie($cookie_name, json_encode($cookie_value), $expire, $path);
+    
+    header("Location: /mylots.php");
+    exit();
+}
+
 
 $layout_content = include_template('layout', [
     'content' => $page_content,
---
+++ mylots.php
@@ -0,0 +1,38 @@
+<?php
+require_once('data.php');
+require_once('functions.php');
+
+
+$my_lots = [];
+
+$cookie_value = [];
+
+if (isset($_COOKIE['bets'])) {
+  $cookie_value = json_decode($_COOKIE['bets'], true);
+}
+
+foreach($cookie_value as $key => $value) {
+  $lot = $announcements[$key];
+  $lot['cost'] = $value['cost'];
+  $lot['time'] = $value['time'];
+  $lot['lot_id'] = $key;
+  $my_lots[] = $lot;
+}
+
+
+$page_content = include_template('mylots', [
+  'categories' => $categories,
+  'my_lots' => $my_lots
+]);
+
+$layout_content = include_template('layout', [
+    'content' => $page_content,
+    'title' => 'Мои ставки',
+    'is_auth' => $is_auth,
+    'user_name' => $user_name,
+    'user_avatar' => $user_avatar
+]);
+
+print($layout_content);
+
+?>
